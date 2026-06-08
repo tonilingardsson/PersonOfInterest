c2060f6019928c46a64e51e1ce37cc8d33700184
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-08 23:26:59 +0200

    Updated Program prepared to install Entity Framework.

--- PersonOfInterest/Program.cs
+++ PersonOfInterest/Program.cs
@@ -1,3 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using PersonOfInterest.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddControllers();
+
+// Add Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add EF Core with SQL server
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
 
 namespace PersonOfInterest
 {
