36d9abac9cba4879ac5b731e279ab302d2863d62
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-08 19:02:25 +0200

    Created the join entity PersonInterest

---
+++ PersonOfInterest/Models/PersonInterest.cs
@@ -0,0 +1,17 @@
+﻿namespace PersonOfInterest.Models
+{
+    public class PersonInterest
+    {
+        // Join entity for many-to-many Person <-> Interest
+        public class PersonInterest
+        {
+            // Foreign key for person object, which it can't be null
+            public int PersonId { get; set; }
+            public Person Person { get; set; } = null!;
+
+            // Foreign key for interet object
+            public int InterestId { get; set; }
+            public Interest Interest { get; set; } = null!;
+        }
+    }
+}
