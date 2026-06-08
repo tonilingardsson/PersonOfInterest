c94304780438132d76fe6ca31f4788dec0e558aa
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-08 18:58:52 +0200

    Created Interest model

---
+++ PersonOfInterest/Models/Interest.cs
@@ -0,0 +1,14 @@
+﻿namespace PersonOfInterest.Models
+{
+    public class Interest
+    {
+        // Basic properties for an Interest
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        // Lists for Interests
+        public List<PersonInterest> PersonInterests { get; set; } = new();
+        public List<Link> Links { get; set; } = new();
+    }
+}
