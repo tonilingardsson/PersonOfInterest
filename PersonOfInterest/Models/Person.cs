9611bac8203a905e6bac215453aa3ed614fa58c7
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-08 18:30:12 +0200

    Created Person model

---
+++ PersonOfInterest/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonOfInterest.Models
+{
+    public class Person
+    {
+        // The personal properties requested
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        // List object for PersonInterest
+        public List<PersonInterest> PersonInterests { get; set; } = new();
+        // List object of Links 
+        public List<Link> Links { get; set; } = new();
+    }
+}
