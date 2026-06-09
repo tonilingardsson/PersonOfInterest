caa14fb204a9b6e1b4ebc9d7ec56ff85b7e12bd4
Antonio Lingårdsson <tonilingardsson@users.noreply.github.com>
2026-06-09 13:50:20 +0200

    Updated Program to render a readable Json result

--- PersonOfInterest/Program.cs
+++ PersonOfInterest/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using PersonOfInterest.Data;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
